e71ac772cbc7e9f53b9a230ca10ad12f6905180a
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2022-02-14 19:31:38 -0300

    feat: adicionar arquivo db com modelo de time

---
+++ src/db.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    nome: "Bahia",
+    cidade: "Salvador",
+    estado: "BA",
+    serie: "B",
+    titulos: {
+      estaduais: 2,
+      nacionais: 1,
+      internacionais: 0,
+    },
+    folhaPagamento: 50000,
+  },
+];
