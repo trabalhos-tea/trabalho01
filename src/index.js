ac1a6d0c73fb32e9514485bd18645068058a78a9
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2022-02-14 19:23:34 -0300

    feat: adicionar arquivo de rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => console.log(`Server running at port ${port}`));
