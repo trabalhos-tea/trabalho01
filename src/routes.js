ed86c9b433d3afaffe05a542f309cc171b80f5d6
Matheus Chaves <larissa.borges@ufpr.br>
2022-02-16 18:48:07 -0300

    feito rota de put e post, para update e cadastro

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,31 @@ routes.get("/", (req, res) => {
 });
 
 routes.post("/newTime", (req, res) => {
+    const {
+      nome,
+      cidade,
+      estado,
+      serie,
+      titulos,
+      folhaPagamento,
+    } = req.body;
 
+    if(nome && cidade && estado !== undefined){
+      const id = timesDb.length + 1
+      timesDb.push({
+        id,
+        nome,
+        cidade,
+        estado,
+        serie,
+        titulos,
+        folhaPagamento,
+      });
+    return res.status(200).json({msg:"Informação atualizada com sucesso"})
+    }
+    else{
+    return res.status(400).json({msg:"Preenchimento de dados infocorreto"})
+    }
 });
 
 routes.get("/:nome", (req, res) => {
@@ -28,7 +52,35 @@ routes.get("/:nome", (req, res) => {
 });
 
 routes.put("/:id", (req, res) => {
-
+  
+    if(isNaN(req.params.id)){
+      return res.sendStatus(400);
+    } else {
+      const id = parseInt(req.params.id);
+      const time = timesDb.find((t) => t.id == id);
+      if(time !=undefined){
+        const {
+          nome,
+          cidade,
+          estado,
+          serie,
+          titulos,
+          folhaPagamento,
+        } = req.body;
+        if(nome != undefined) time.nome = nome;
+        if(cidade != undefined) time.cidade = cidade;
+        if(estado != undefined) time.estado = estado;
+        if(serie != undefined) time.serie = serie;
+        if(titulos.estaduais != undefined) time.titulos.estaduais = titulos.estaduais;
+        if(titulos.nacionais != undefined) time.titulos.nacionais = titulos.nacionais;
+        if(titulos.internacionais != undefined) time.titulos.internacionais = titulos.internacionais;
+        if(folhaPagamento != undefined) time.folhaPagamento = folhaPagamento;
+        return res.status(200).json({msg: "Time atualizado com sucesso"});
+      }
+      else{
+        return res.status(400).json({msg:"Id não encontrado"})
+      }
+    }
 });
 
 routes.delete("/:id", (req, res) => {
